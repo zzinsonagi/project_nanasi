f59287591faf03c37db476ab32c8726006ee6883
sonagiing <sonagiing@naver.com>
2024-03-12 00:16:23 +0900

    feat:hr_VO setting

---
+++ src/main/java/com/nanasi/NanasiApplication.java
@@ -0,0 +1,13 @@
+package com.nanasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanasiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nanasi/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.nanasi.controller;
+
+import org.springframework.stereotype.Controller;
+import com.nanasi.common.MailSenderRunner;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+	
+	private final MailSenderRunner mailsend;
+	
+	//로그인 페이지
+
+	//회원등록
+
+	//중복 회원 확인
+	//0 : 중복 無,		1 : 중복 有
+
+	//약관 페이지 접속
+	
+	//인증 메일
+	
+}
