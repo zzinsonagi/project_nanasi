3f843f530d455b6dda50fdc788793aaadf43bc5e
reyasya <hyslsg9408@gmail.com>
2024-03-12 17:18:16 +0900

    dasi commit

--- src/main/java/com/nanasi/controller/TestController.java
+++ src/main/java/com/nanasi/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.nanasi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test")
 public class TestController {
 
 }
